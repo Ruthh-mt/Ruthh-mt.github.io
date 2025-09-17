887b0f149a08a4132127f852e9319a9e7bad3cf6
Aereises <ruthmetayer0@gmail.com>
2025-09-17 21:29:01 +0200

    feat: creation du debut du portfolio pour le lien [Work in progress]

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+    <html lang="fr">
+        <head>
+            <meta charset="utf-8">
+            <title>Portfolio</title>
+        </head>
+        <body>
+            <h1>Portfolio</h1>
+            <h2>Bienvenu sur mon portfolio</h2>
+            <p>This part of the map has not been unlocked yet please gain more xp to unlock</p>
+        </body>
+    </html>
+
